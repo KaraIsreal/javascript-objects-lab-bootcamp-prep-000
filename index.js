6216e849ec39a37fd6b77f0ac3feb1c751a563da
Kara Isreal <isreal.kara92@gmail.com>
2018-09-09 04:22:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){
   const recipes2 = Object.assign({}, object, {[key]: value})
   return recipes2
+}
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key]= value;
+  return object
 }
