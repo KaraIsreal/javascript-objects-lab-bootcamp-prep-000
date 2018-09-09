f501f7e9d01f5015e02871cdc9a95a94bad52c05
Kara Isreal <isreal.kara92@gmail.com>
2018-09-09 04:12:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {}
 
-var newObject = Object.assign({}, object, ['key: value'])
+const newObject = Object.assign({}, object, {['key: value']})
 
 var object = { foo: 'bar' }
 
 function updateObjectWithKeyAndValue(object, key, value){
-  const newObject = Object.assign({}, object, ['key: value'])
+  object[key] = value
 return newObject
 }
 
