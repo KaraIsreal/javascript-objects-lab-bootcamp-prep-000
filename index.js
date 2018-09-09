a006250078d28d06f7b7a1ea42f7982c3efd571d
Kara Isreal <isreal.kara92@gmail.com>
2018-09-09 04:31:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key){
+  var object = { foo: 'bar' }
   delete object[key];
   return object
 }
