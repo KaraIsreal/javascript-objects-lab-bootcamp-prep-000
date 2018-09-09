c353dee3bd902a647a7e6bba4120e898e5b7d90b
Kara Isreal <isreal.kara92@gmail.com>
2018-09-09 04:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const newObject = Object.assign({}, object, {['key: value']})
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return newObject = Object.assign({}, object, {['key: value']})
+  return Object.assign({}, object, {['key: value']})
 
 }
 
